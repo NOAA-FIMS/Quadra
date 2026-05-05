ccaf7ff679c3e034ca48ee893a81e5e94da9579f
Matthew <matthew.supernaw@noaa.gov>
2026-05-05 11:42:08 -0400

    updated scaled test

--- main.cpp
+++ main.cpp
@@ -229,7 +229,7 @@ struct AR1REModel
 
 void test_scaled()
 {
-    std::vector<int> sizes = {1, 2, 5, 10, 25, 50, 100, 200, 500, 1000, 100000};
+    std::vector<int> sizes = {1, 2, 5, 10, 25, 50, 100, 200, 500, 1000, 10000};
     int run = 1;
     for (int size : sizes)
     {
