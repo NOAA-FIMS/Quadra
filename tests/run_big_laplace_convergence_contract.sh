aaf307f0b8135347c01b1a209e09be02d68cbe5a
Matthew <matthew.supernaw@noaa.gov>
2026-05-18 21:06:21 -0400

    Fix Eigen include path in big Laplace contract

--- tests/run_big_laplace_convergence_contract.sh
+++ tests/run_big_laplace_convergence_contract.sh
@@ -5,7 +5,7 @@ set -x
 OUT=tests/big_laplace_convergence_contract.out
 BUILD_LOG=tests/big_laplace_convergence_contract.build.log
 
-if ! make run-big-examples CXXFLAGS="-std=c++17 -O3 -flto -I. -I./external/eigen -I./external/had -I./external/LBFGSpp/include" > "$BUILD_LOG" 2>&1; then
+if ! make run-big-examples CXXFLAGS="-std=c++17 -O3 -flto -I. -I./external/eigen -I./external/eigen/Eigen -I./external/had -I./external/LBFGSpp/include" > "$BUILD_LOG" 2>&1; then
   echo "ERROR: make run-big-examples failed"
   cat "$BUILD_LOG"
   exit 2
