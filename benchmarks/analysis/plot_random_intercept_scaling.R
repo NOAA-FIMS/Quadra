c939fe62a45ef2bec8c367cc319f964a9a6615e0
Matthew <matthew.supernaw@noaa.gov>
2026-05-25 15:51:06 -0400

    Differentiate random-intercept benchmark plot symbols

--- benchmarks/analysis/plot_random_intercept_scaling.R
+++ benchmarks/analysis/plot_random_intercept_scaling.R
@@ -29,21 +29,28 @@ plot(
 
 engines <- unique(df$engine)
 
-for (eng in engines) {
+pch_values <- c(1, 2, 3, 4, 5, 6)
+lty_values <- c(1, 2, 3, 4, 5, 6)
+
+for (i in seq_along(engines)) {
+  eng <- engines[i]
   idx <- df$engine == eng
+  ord <- order(df$n_obs[idx])
 
   lines(
-    df$n_obs[idx],
-    df$total_wall_ms[idx],
-    type = "b"
+    df$n_obs[idx][ord],
+    df$total_wall_ms[idx][ord],
+    type = "b",
+    pch = pch_values[i],
+    lty = lty_values[i]
   )
 }
 
 legend(
   "topleft",
   legend = engines,
-  lty = 1,
-  pch = 1
+  lty = lty_values[seq_along(engines)],
+  pch = pch_values[seq_along(engines)]
 )
 
 dev.off()
